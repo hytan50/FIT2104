97ac76214a5a55c4bc5faf5bb8598ffb50439992
hytan50 <hytan50@gmail.com>
2017-09-24 14:00:56 +1000

    Move all sql code to images/includes/functions.php delete.php: show the messages for 10 seconds instead of 7

--- Ass2/images/delete.php
+++ Ass2/images/delete.php
@@ -1,35 +1,23 @@
 <?php
     include_once("../config.php");
+    include_once ("includes/functions.php");
     $filepath = "/Ass2/schema/images/";     //subject to change to "product_image" folder
     //$dir = opendir($filepath);
     if(!empty($_POST['check'])){
         $checkbox = $_POST['check'];
         foreach ($checkbox as $check) {
             //notify user that the affected product for the image
-            $query = "SELECT * FROM product_image WHERE name = ?";
-            $pquery = $conn->prepare($query);
-            $pquery->bind_param("s", $check);
-            $pquery->execute();
-            $result = $pquery->get_result();
-            $row = $result->fetch_assoc();
+            $row = getProductDescription($check);
             if ($row) {
-                $squery = "SELECT * FROM product WHERE id = ?";
-                $spquery = $conn->prepare($squery);
-                $spquery->bind_param("i", $row["product_id"]);
-                $spquery->execute();
-                $sresult = $spquery->get_result();
-                $srow = $sresult->fetch_assoc();
-
+                $srow = getProduct($row["product_id"]);
                 echo "<h4>".$check." is currently use by: " . $srow['name'] . ". Product id = " . $srow["id"] . "</h4>";
             }
             else{
                 echo "<h4>".$check." does not used by any product yet.</h4>";
             }
             //delete from database
-            $query = "DELETE FROM product_image WHERE name=?";
-            $pquery = $conn->prepare($query);
-            $pquery->bind_param("s", $check);
-            $pquery->execute();
+            deleteProductImage($check);
+
             //delete from directory
             try{
                 unlink($_SERVER['DOCUMENT_ROOT'].$filepath.$check);
@@ -40,6 +28,6 @@
             }
         }
 
-        header("refresh:7; url=list.php");
+        header("refresh:10; url=list.php");
     }
 ?>
--- Ass2/images/includes/functions.php
+++ Ass2/images/includes/functions.php
@@ -8,3 +8,21 @@
       $result = $pquery->get_result();
       return $result->fetch_assoc();
   }
+
+  function getProduct($id){
+      global $conn;
+      $query = "SELECT * FROM product WHERE id = ?";
+      $pquery = $conn->prepare($query);
+      $pquery->bind_param("i", $id);
+      $pquery->execute();
+      $result = $pquery->get_result();
+      return $result->fetch_assoc();
+  }
+
+  function deleteProductImage($name){
+      global $conn;
+      $query = "DELETE FROM product_image WHERE name=?";
+      $pquery = $conn->prepare($query);
+      $pquery->bind_param("s", $name);
+      return $pquery->execute();
+  }
